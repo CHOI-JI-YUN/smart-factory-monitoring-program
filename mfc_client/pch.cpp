f5b65b41ac26e619637c8ebd1ba2a1903e445aa1
CHOI-JI-YUN <113336861+CHOI-JI-YUN@users.noreply.github.com>
2026-07-09 15:31:37 +0900

    Delete mfc_client directory

--- mfc_client/pch.cpp
+++
@@ -1,5 +0,0 @@
-﻿// pch.cpp: source file corresponding to the pre-compiled header
-
-#include "pch.h"
-
-// When using pre-compiled headers, this source file is necessary for compilation to succeed.
